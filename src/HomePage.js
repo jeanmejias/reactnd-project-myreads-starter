391ba415b0e6c75adac968d0988497b5cd64ebce
jeanmejias <jean.eg.mejias@hotmail.com>
2018-08-20 11:58:24 +0100

    update the HomePage

--- src/HomePage.js
+++ src/HomePage.js
@@ -24,8 +24,49 @@ class HomePage extends Component {
                                               shelf={book.shelf}/>
                                           )
                                       } 
+                                      </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {this.props.books
+                      .filter((book) => book.shelf === 'wantToRead')
+                      .map((book) =>
+                        <Book key={book.id}
+                        updateShelf={this.props.updateShelf}
+                        book={book}
+                        shelf={book.shelf}/>
+                      )
+                    }
+                  </ol>
+                </div>
+              </div>
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {this.props.books
+                        .filter((book) => book.shelf === 'read')
+                        .map((book) =>
+                          <Book key={book.id}
+                          updateShelf={this.props.updateShelf}
+                          book={book}
+                          shelf={book.shelf}/>
+                        )
+                      }
+                    </ol>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="open-search">
+              <Link to="/search">Add a book</Link>
+            </div>
+      </div>
         );
     }
 }
 
-export default MainPage;
+export default HomePage;
