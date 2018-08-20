6086f3825d5b558de6c1a6462f9503cb29813362
jeanmejias <jean.eg.mejias@hotmail.com>
2018-08-20 18:01:49 +0100

    Updated

--- src/Book.js
+++ src/Book.js
@@ -1,30 +1,52 @@
-import React from 'react'
+import React, {Component} from 'react';
 
-class Book extends React.Component {
 
-  render () {
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : ''}"` }}></div>
-          <div className="book-shelf-changer">
-            <select
-              onChange={(event) => this.props.updateShelf(this.props.book, event.target.value)}
-              value={this.props.shelf}>
-              <option value="move" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
-        </div>
-        <div className="book-title">{this.props.book.title}</div>
-        <div className="book-authors">{this.props.book.authors}</div>
-      </div>
-    );
-  }
+class Book extends Component {
+
+    render(){
+
+       // console.log(this.props.book.shelf)
+        let {cover,
+            book,
+            title,
+            authors,
+            shelf,
+            selectOptions
+            } = this.props;
+
+        return(
+            <div className="book">
+                <div className="book-top">
+                <div className="book-cover" 
+                    style={{ width: 128,
+                        height: 193,
+                        backgroundImage: `url(${cover ? book.imageLinks.thumbnail : 'No Book cover'})`}}>
+                </div>
+
+                <div className="book-shelf-changer">
+                    <select
+                        value = {shelf||'none'}
+                        onChange = {(event) =>
+                            {selectOptions(book, event.target.value)}
+                        }
+                    >
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                    
+                    </select>
+                </div>
+
+                </div>
+
+                <div className="book-title">{title}</div>
+                <div className="book-authors">{authors}</div>
+
+            </div>
+        );
+    }
 }
 
-
-export default Book
+export default Book;
