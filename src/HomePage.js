3727738dcb331c4730ba03cc22be3d4fbc74ec4c
jeanmejias <jean.eg.mejias@hotmail.com>
2018-08-20 01:33:43 +0100

    HomePage created

---
+++ src/HomePage.js
@@ -0,0 +1,11 @@
+import React, {Component } from 'react';
+import Book from "./Book";
+import {Link} from 'react-router-dom'
+
+class HomePage extends Component {
+    render () {
+        return ( 
+            
+        );
+    }
+}
