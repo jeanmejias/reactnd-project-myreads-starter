54164522c4fdaab279b04cf5ea97032241eec47e
jeanmejias <jean.eg.mejias@hotmail.com>
2018-08-20 11:51:28 +0100

    Updating the book shelf

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,7 +5,27 @@ import {Link} from 'react-router-dom'
 class HomePage extends Component {
     render () {
         return ( 
-            
+            <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                      {this.props.books
+                        .filter((book) => book.shelf === 'currentlyReading')
+                        .map((book) =>
+                          <Book key={book.id}
+                                              updateShelf={this.props.updateShelf}
+                                              book={book}
+                                              shelf={book.shelf}/>
+                                          )
+                                      } 
         );
     }
 }
+
+export default MainPage;
